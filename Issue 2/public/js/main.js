9766e8ae7ac64de2e15d557060d5381ba541ada5
FrozonFreak <shazcool@gmail.com>
2014-07-05 19:36:35 +0800

    Convert CSS to Stylus
    Stylus broken since expressjs stylus module does not support complete
    syntax

---
+++ Issue 2/public/js/main.js
@@ -0,0 +1,18 @@
+require.config({
+    paths: {
+                //tries to load jQuery from Google's CDN first and falls back
+                //to load locally
+        "jquery": "libs/jquery/jquery",
+        "underscore": "../bower_components/underscore/underscore",
+        "backbone": "../bower_components/backbone/backbone",
+        "backbone.localStorage": "../bower_components/backbone.localStorage/backbone.localStorage"
+    },
+    shim: {
+        "backbone": {
+                        //loads dependencies first
+            deps: ["jquery", "underscore"],
+                        //custom export name, this would be lowercase otherwise
+            exports: "Backbone"
+        }
+    }
+});
